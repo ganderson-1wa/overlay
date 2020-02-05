4c9e21ee811fe678e6f7e969c6c6d5e896d4f0cc
Greg Anderson <ganderson@1wa.org>
2020-02-05 14:49:53 -0800

    responsive controls and adjusted grid

--- js/vimeo.js
+++ js/vimeo.js
@@ -57,24 +57,47 @@ $(function() {
 
 });
 
-$('#zoom-01').bind('click', function () {
-    var zoomLevel1 = $('.content-vimeo');
+$('#zoom-00').bind('click', function () {
+    var zoomLevel1 = $('.content-vimeo-zoom-01');
     var zoomLevel2 = $('.content-vimeo-zoom-02');
+    var control1 = $('.controls-zoom-01');
+    var control2 = $('.controls-zoom-02');
     
     if (zoomLevel1)
-    zoomLevel1.removeClass('content-vimeo').addClass('content-vimeo-zoom-01');
+    zoomLevel1.removeClass('content-vimeo-zoom-01').addClass('content-vimeo');
+    control1.removeClass('controls-zoom-01').addClass('controls');
     if (zoomLevel2)
     zoomLevel2.removeClass('content-vimeo-zoom-02').addClass('content-vimeo');
+    control2.removeClass('controls-zoom-02').addClass('controls');
     
+});
+
+$('#zoom-01').bind('click', function () {
+    var zoomLevel0 = $('.content-vimeo');
+    var zoomLevel2 = $('.content-vimeo-zoom-02');
+    var control0 = $('.controls');
+    var control2 = $('.controls-zoom-02');
+    
+    if (zoomLevel0)
+    zoomLevel0.removeClass('content-vimeo').addClass('content-vimeo-zoom-01');
+    control0.removeClass('controls').addClass('controls-zoom-01');
+    if (zoomLevel2)
+    zoomLevel2.removeClass('content-vimeo-zoom-02').addClass('content-vimeo-zoom-01');
+    control2.removeClass('controls-zoom-02').addClass('controls-zoom-01');
 });
 
 $('#zoom-02').bind('click', function () {
+    var zoomLevel0 = $('.content-vimeo');
     var zoomLevel1 = $('.content-vimeo-zoom-01');
-    var zoomLevel2 = $('.content-vimeo');
-
-    if (zoomLevel2)
-        zoomLevel2.removeClass('content-vimeo').addClass('content-vimeo-zoom-02');
+    var control0 = $('.controls');
+    var control1 = $('.controls-zoom-01');
+    
+    if (zoomLevel0)
+    zoomLevel0.removeClass('content-vimeo').addClass('content-vimeo-zoom-02');
+    control0.removeClass('controls').addClass('controls-zoom-02');
     if (zoomLevel1)
-    zoomLevel1.removeClass('content-vimeo-zoom-01').addClass('content-vimeo');
+    zoomLevel1.removeClass('content-vimeo-zoom-01').addClass('content-vimeo-zoom-02');
+    control1.removeClass('controls-zoom-01').addClass('controls-zoom-02');
 
 });
+
